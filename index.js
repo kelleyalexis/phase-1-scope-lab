1ba969a83009601b3169b1685fbde5f1bb4e7644
Kelley Shackelford <kelleyalexis@yahoo.com>
2024-03-11 16:48:26 -0400

    Learning scope

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    window.customerName = window.customerName.toUpperCase();
+    console.log(customerName);
+}
+
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+}
+
+var overwriteCustomer = bestCustomer
+function overwriteBestCustomer() {
+    window.bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'linda'
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'not linda';
+}
+
